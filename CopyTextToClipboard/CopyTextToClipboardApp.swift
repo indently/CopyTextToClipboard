57437ece7554d205dabcdaa83fe27a994813d480
Federico <codepalace@fastmail.se>
2022-03-22 09:57:45 +0100

    Initial Commit

---
+++ CopyTextToClipboard/CopyTextToClipboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  CopyTextToClipboardApp.swift
+//  CopyTextToClipboard
+//
+//  Created by Federico on 22/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct CopyTextToClipboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
