b95dc0ac93afd2145370b927387dcb8d281fe41c
Federico <codepalace@fastmail.se>
2022-03-22 10:32:42 +0100

    First

--- CopyTextToClipboard/ContentView.swift
+++ CopyTextToClipboard/ContentView.swift
@@ -6,11 +6,50 @@
 //
 
 import SwiftUI
+import UIKit
 
 struct ContentView: View {
+    @State private var text: String = ""
+    @State private var buttonText  = "Copy to clipboard"
+    private let pasteboard = UIPasteboard.general
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            TextField("Insert text here", text: $text)
+                .textFieldStyle(.roundedBorder)
+            
+            HStack {
+                Button {
+                    copyToClipboard()
+                } label: {
+                    Label(buttonText, systemImage: "doc.on.doc.fill")
+                }
+                .tint(.orange)
+                Spacer()
+                Button {
+                    paste()
+                } label: {
+                    Label("Paste", systemImage: "doc.on.clipboard")
+                }
+            }
+        }
+        .padding()
+    }
+    
+    func paste(){
+        if let string = pasteboard.string {
+            text = string
+        }
+    }
+    
+    func copyToClipboard() {
+        pasteboard.string = self.text
+        
+        self.buttonText = "Copied!"
+        self.text = ""
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.5) {
+            self.buttonText = "Copy to clipboard"
+        }
     }
 }
 
